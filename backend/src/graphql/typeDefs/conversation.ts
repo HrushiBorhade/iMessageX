1d96fd2f4b9e5a74ab94922d93cd537f987bbdfa
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-02-18 17:56:51 +0530

    created data models of Conversation , ConversationParticipant and message in prisma schema and generated new schema

--- backend/src/graphql/typeDefs/conversation.ts
+++ backend/src/graphql/typeDefs/conversation.ts
@@ -1,58 +1,9 @@
-import gql from "graphql-tag";
-
+import { gql } from "apollo-server-core";
 const typeDefs = gql`
   type Mutation {
     createConversation(participantIds: [String]): CreateConversationResponse
   }
-
-  # type Mutation {
-  #   markConversationAsRead(userId: String!, conversationId: String!): Boolean
-  # }
-
-  # type Mutation {
-  #   deleteConversation(conversationId: String!): Boolean
-  # }
-
   type CreateConversationResponse {
-    conversationId: String
+    conversationId: string
   }
-
-  # type ConversationUpdatedSubscriptionPayload {
-  #   conversation: Conversation
-  # }
-
-  # type ConversationDeletedSubscriptionPayload {
-  #   id: String
-  # }
-
-  # type Conversation {
-  #   id: String
-  #   latestMessage: Message
-  #   participants: [Participant]
-  #   updatedAt: Date
-  # }
-
-  # type Participant {
-  #   id: String
-  #   user: User
-  #   hasSeenLatestMessage: Boolean
-  # }
-
-  # type Query {
-  #   conversations: [Conversation]
-  # }
-
-  # type Subscription {
-  #   conversationCreated: Conversation
-  # }
-
-  # type Subscription {
-  #   conversationUpdated: ConversationUpdatedSubscriptionPayload
-  # }
-
-  # type Subscription {
-  #   conversationDeleted: ConversationDeletedSubscriptionPayload
-  # }
 `;
-
-export default typeDefs;
